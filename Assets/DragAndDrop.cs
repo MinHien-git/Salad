8982969dc7c241c9943be620328774127b9e4389
hien.luong <hien.luong@opswat.com>
2024-09-23 08:01:05 +0700

    update

--- Assets/DragAndDrop.cs
+++ Assets/DragAndDrop.cs
@@ -2,32 +2,49 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class DragAndDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class DragAndDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerDownHandler
 {
     public Transform parentAfterDrag;
     public Image image;
     public IngredientPlate plate;
+
+    private Vector2 offset;
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         parentAfterDrag = transform.parent;
         transform.SetParent(transform.root);
         transform.SetAsLastSibling();
-        Vector2 temp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         image.raycastTarget = false;
 
-        transform.position = temp;
+        // Adjust position to account for the offset between the pointer/touch and the object center
+        Vector2 pointerPosition = GetPointerPosition(eventData);
+        transform.position = pointerPosition;
+        Debug.Log(transform.position);
     }
-
     public void OnDrag(PointerEventData eventData)
     {
-        Vector2 temp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = temp;
+        Vector2 pointerPosition = GetPointerPosition(eventData);
+        transform.position = pointerPosition;
+        Debug.Log(transform.position);
     }
 
+    public void OnPointerDown(PointerEventData eventData)
+    {
+         Vector2 pointerPosition = GetPointerPosition(eventData);
+        transform.position = pointerPosition + offset;
+        Debug.Log(transform.position);
+    }
+    
     public virtual void OnEndDrag(PointerEventData eventData)
     {
         transform.SetParent(parentAfterDrag);
         image.raycastTarget = true;
+    }
+
+    private Vector2 GetPointerPosition(PointerEventData eventData)
+    {
+        Vector2 pointerPosition = Camera.main.ScreenToWorldPoint(eventData.position);
+        return pointerPosition;
     }
 }
